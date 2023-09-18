4604501d394b01ed89b4c005d9d8c048d29bb642
Jan Klaban <jan.klaban@ddash.cz>
2023-09-18 18:22:27 +0200

    table

--- sampleapi/MyController.cs
+++ sampleapi/MyController.cs
@@ -23,34 +23,43 @@ namespace sampleapi
         {
             var ret = new List<SimpleMessage>();
             await using var db = await _dbsource.OpenConnectionAsync();
-            using var cmd = db.CreateCommand();
-            cmd.CommandText = "select ts,msg from messages";
+            await using var cmd = db.CreateCommand();
+            cmd.CommandText = "select id,username,message,created_at from messages";
             await using var reader = await cmd.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
                 ret.Add(new SimpleMessage
                 {
-                    Message = reader.GetString(1),
-                    Timestamp = reader.GetDateTime(0)
+                    Id = reader.GetInt64(0),
+                    Username = reader.GetString(1),
+                    Message = reader.GetString(2),
+                    CreatedAt = reader.GetDateTime(3)
                 });
             }
-            return new OkObjectResult(ret);
+            return Ok(ret);
         }
 
         [HttpPost("somepost")]
         [Produces("application/json")]
-        public async Task<IActionResult> SomePost([FromBody][Required] string msg)
+        public async Task<IActionResult> SomePost([FromQuery][Required] string username, [FromBody][Required] string msg)
         {
             if (string.IsNullOrWhiteSpace(msg))
                 return BadRequest("Msg is empty");
+            if (string.IsNullOrWhiteSpace(username))
+                return BadRequest("Username is empty");
 
             await using var db = await _dbsource.OpenConnectionAsync();
-            using var cmd = db.CreateCommand();
-            cmd.CommandText = "insert into messages (ts,msg) values (:ts,:msg)";
+            await using var cmd = db.CreateCommand();
+            cmd.CommandText = "insert into messages (created_at,message,username) values (:ts,:msg,:user) returning id";
             cmd.Parameters.AddWithValue("ts", DateTime.UtcNow);
             cmd.Parameters.AddWithValue("msg", msg);
-            await cmd.ExecuteNonQueryAsync();
-            return Ok();
+            cmd.Parameters.AddWithValue("user", username);
+
+            await using var reader = await cmd.ExecuteReaderAsync();
+            if (!await reader.ReadAsync())
+                return StatusCode(500);
+
+            return Ok(reader.GetInt64(0));
         }
     }
 }
--- sampleapi/SimpleMessage.cs
+++ sampleapi/SimpleMessage.cs
@@ -4,7 +4,9 @@ namespace sampleapi
 {
     public class SimpleMessage
     {
+        public long Id { get; set; }
+        public string? Username { get; set; }
         public string? Message { get; set; }
-        public DateTime Timestamp { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
